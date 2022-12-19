9529c0e6fd8492647938166bf041ba44d53d0fcd
aleksandra <alexandra.bulgak@gmail.com>
2022-12-19 13:28:46 +0200

    add Task 010 The Second Number

---
+++ Task010_TheSecondNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число 
+// и на выходе показыввет вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+string str = number.ToString();
+if (str.Length == 3)
+{
+    int[] array = new int[3];
+    for (int i = 0; i < 3; i++)
+    {
+        array[i] = int.Parse(str[i].ToString());
+    }
+    Console.WriteLine(array[1]);
+}
+else
+    Console.WriteLine("Вы ввели неверное число. Пожадуйста, введите трехзначное число");
