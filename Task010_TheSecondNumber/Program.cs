7e4249d5c95b224b1d371aede7bec9c6399477eb
aleksandra <alexandra.bulgak@gmail.com>
2022-12-19 13:58:00 +0200

    update

--- Task010_TheSecondNumber/Program.cs
+++ Task010_TheSecondNumber/Program.cs
@@ -6,14 +6,6 @@ int number = Convert.ToInt32(Console.ReadLine());
 number = Math.Abs(number);
 
 string str = number.ToString();
-if (str.Length == 3)
-{
-    int[] array = new int[3];
-    for (int i = 0; i < 3; i++)
-    {
-        array[i] = int.Parse(str[i].ToString());
-    }
-    Console.WriteLine(array[1]);
-}
+if (str.Length == 3) Console.WriteLine(str[1]);
 else
-    Console.WriteLine("Вы ввели неверное число. Пожадуйста, введите трехзначное число");
+    Console.WriteLine("Вы ввели неверное число. Пожалуйста, введите трехзначное число");
