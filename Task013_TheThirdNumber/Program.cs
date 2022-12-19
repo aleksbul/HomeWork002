340f68aa9bda3f54b237ce00fc6baeca8a5d258f
aleksandra <alexandra.bulgak@gmail.com>
2022-12-19 13:59:08 +0200

    update

--- Task013_TheThirdNumber/Program.cs
+++ Task013_TheThirdNumber/Program.cs
@@ -6,14 +6,6 @@ int number = Convert.ToInt32(Console.ReadLine());
 number = Math.Abs(number);
 
 string str = number.ToString();
-if (str.Length >= 3)
-{
-    int[] array = new int[str.Length];
-    for (int i = 0; i < str.Length; i++)
-    {
-        array[i] = int.Parse(str[i].ToString());
-    }
-    Console.WriteLine(array[2]);
-}
+if (str.Length >= 3) Console.WriteLine(str[2]);
 else
     Console.WriteLine("В заданном числе третьей цифры нет");
